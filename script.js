89e44f92f67151efdc8bbaee4c145015624a001b
Faithfuloscar <oscarmendez2003@outlook.com>
2024-10-23 18:59:00 -0700

    completed lecture activity

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// this is a comment-line 
+/* this 
+is 
+multi
+line
+comment
+*/
 console.log("hello world!")
+
+console.log(8 * null); //result is zero
+
+console.log("5" - 1) // result is 4
+
+console.log("5" + 1) // result is 51
+
+console.log("five" * 2) // result is NaN
+
+console.log(5 > 2 && 6 > 3) // result is true
+
+console.log(false == false ? "equal" : "not equal") //result is equal
+
+console.log(true || false) // result is true
